0c252a4d0baf01cec922fb7fbb088ecb96faaec1
Marcelo <mvarg005@odu.edu>
2024-11-22 12:35:03 -0500

    add comments to explain how the Quadratic Equation class works.

--- models/quadratic.py
+++ models/quadratic.py
@@ -1,8 +1,19 @@
 class QuadraticEquation:
     def __init__(self, a, b, c):
+        """
+        Initialize a QuadraticEquation object.
+
+        Args:
+        - a (int or float): Coefficient of x^2
+        - b (int or float): Coefficient of x
+        - c (int or float): Constant term
+        """
         self.a = a
         self.b = b
         self.c = c
 
     def __repr__(self):
+        """
+        Return a string representation of the quadratic equation.
+        """
         return f"{self.a}x^2 + {self.b}x + {self.c}"
